1de9d7caf84c65d9cad4668ac4838be1977cdbcc
Miles Hochstein <miles.hochstein@gmail.com>
2015-01-27 14:00:00 -0800

    set up for using main index.js to supply function getPostFiles

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-//index.js for static content
+//index.js for gathering jade files from directory and returning array of their content
+var express = require('express');
+//var staticlib = require('staticSiteFun');
+//var router =  express.Router();
 
-var express - require('express');
-var staticlib = require('staticSiteFun');
-var router =  express.Router();
+///give contentFilepath as argument when calling gatherPostFiles
+var gatherPostFiles = function(arg){ 
+	var contentFilepath = arg;
+ 	var blogPostFiles = fs.readdirSync(__dirname + contentFilepath ); 
+	var collector=[];      //collects file names
+	var fileCollector=[];  //collects file contents
+	// regex find all .jade content files in posts directory 
+	blogPostFiles.forEach(function cleanFiles (value, index, array){
+		var infile;  //takes fs incoming file
+		var pattern = new RegExp(".jade");   //match file .jade
+		if ( pattern.test(value)  )  // file IS dot jade
+			{
+			inFile= fs.readFileSync( __dirname + contentFilepath + blogPostFiles[index]  );
+			inFile = jade.render(inFile);   //make jade into html
+			fileCollector.push(inFile);    //keep file content		
+			collector.push(value);         //keep file name, not really needed?
+			//console.log("Is a dot jade file.");
+			}
+		// else { //console.log("DID NOT FIND dot jade FILE."); }
+	return fileCollector;
+	});
+	//console.log(" collector is " + collector);
+	//console.log(collector.length + " is length");
+	//console.log("fileCollector is " + fileCollector);
+	//console.log(fileCollector.length + " is fileCollector length");
+	console.log("remote access of gatherPostFiles");
+return fileCollector;
+};  //close gatherPostFiles
 
-module.exports = staticlib.generatedRoutes(router);
+module.exports = fileimport;
+
+
