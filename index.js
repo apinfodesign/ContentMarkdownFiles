f84d413fdac2a10d5124f1a1f9b071a6a642c9f8
Miles Hochstein <miles.hochstein@gmail.com>
2015-02-01 20:48:32 -0800

    clean up configuraiton JSON file and add back json parse

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ var gatherConfigurationInfo = function(arg){
 	var configureFilePath = arg;
 //var outfile="gatherCofigurationInfo happened";
 	console.log("reading gatherConfigurationInfo and configurePath is"+ configureFilePath);
-	configCollector =   fs.readFileSync(configureFilePath + "siteConfigurationUser.json");
+	configCollector =  
+	     JSON.parse(fs.readFileSync(configureFilePath + "siteConfigurationUser.json"));
  // 	console.log (inFile.siteName + " " + inFile.siteNameSubtitle);
  // console.log( configInfo );
 	return configCollector;
